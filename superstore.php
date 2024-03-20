22a4a81d9ca7d0c104a656263022bb3f871caae0
nyiaware <143770139+nyiaware@users.noreply.github.com>
2024-03-19 20:33:14 -0400

    Update superstore.php

--- superstore.php
+++ superstore.php
@@ -1,135 +1,89 @@
 <?php
-function generateRandomString($length = 10) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
-    }
-    return $randomString;
+function readFromFile($filename) {
+    $lines = file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    return $lines;
 }
 
+// read data from text files
+$streetTypes = readFromFile('street_types.txt');
+$domains = readFromFile('domains.txt');
+$firstNames = readFromFile('first_names.txt');
+$lastNames = readFromFile('last_names.txt');
+$streetNames = readFromFile('street_names.txt');
+$states = readFromFile('states.txt');
+$cities = readFromFile('cities.txt');
+$products = readFromFile('products.txt');
 
-function generateRandomFullName() {
-    $firstName = generateRandomString(rand(5, 10));
-    $lastName = generateRandomString(rand(5, 10));
-    return $firstName . ' ' . $lastName;
-}
-
-
-function generateRandomEmail($firstName, $lastName) {
-    $domains = ['gmail.com', 'yahoo.com', 'hotmail.com', 'example.com'];
-    $randomDomain = $domains[array_rand($domains)];
-    return strtolower($firstName . '.' . $lastName . '@' . $randomDomain);
-}
-
-
-function generateRandomPhoneNumber() {
-    return '555-' . rand(100, 999) . '-' . rand(1000, 9999);
-}
-
-
-function generateRandomAddress() {
-    $streetNumber = rand(1, 1000);
-    $streetName = generateRandomString(rand(5, 10)) . ' St';
-    $city = generateRandomString(rand(5, 10));
-    $states = ['NY', 'CA', 'TX', 'FL', 'IL'];
-    $state = $states[array_rand($states)];
-    $zip = rand(10000, 99999);
-    return [$streetNumber, $streetName, $city, $state, $zip];
-}
-
-
-function generateProductDescription() {
-    $adjectives = ['wonderful', 'amazing', 'fantastic', 'awesome', 'excellent'];
-    $nouns = ['hammer', 'screwdriver', 'chisel', 'pliers', 'axe'];
-    $adjective = $adjectives[array_rand($adjectives)];
-    $noun = $nouns[array_rand($nouns)];
-    $colors = ['red', 'blue', 'green', 'yellow', 'black'];
-    $color = $colors[array_rand($colors)];
-    $materials = ['wood', 'metal', 'plastic', 'steel', 'aluminum'];
-    $material = $materials[array_rand($materials)];
-    $verbs = ['hammering', 'screwing', 'fixing', 'building', 'repairing'];
-    $verb = $verbs[array_rand($verbs)];
-    return "A $adjective $noun in an $color made of $material useful for $verb.";
-}
-
-
-function generateRandomWeight() {
-    return rand(1, 100) . ' lbs';
-}
-
-
-function generateRandomBaseCost() {
-    return rand(10, 100) . '.99';
-}
-
-
-$dumpSuperstoreSqlContent = file_get_contents('dump-superstore.sql');
-
-
+// generate data for addresses
 $addressData = '';
-for ($i = 1; $i <= 150; $i++) {
-    $address = generateRandomAddress();
-    $addressData .= "INSERT INTO address (street, city, state, zip) VALUES ('{$address[0]} {$address[1]}', '{$address[2]}', '{$address[3]}', '{$address[4]}');\n";
+foreach ($streetTypes as $streetType) {
+    foreach ($streetNames as $streetName) {
+        $city = $cities[array_rand($cities)];
+        $state = $states[array_rand($states)];
+        $zip = rand(10000, 99999);
+        $addressData .= "INSERT INTO address (street, city, state, zip) VALUES ('$streetType $streetName', '$city', '$state', '$zip');\n";
+    }
 }
 
-
+// generate data for customers
 $customerData = '';
-for ($i = 1; $i <= 100; $i++) {
-    $fullName = generateRandomFullName();
-    $email = generateRandomEmail(explode(' ', $fullName)[0], explode(' ', $fullName)[1]);
-    $phone = generateRandomPhoneNumber();
-    $addressId = rand(1, 150);
-    $customerData .= "INSERT INTO customer (first_name, last_name, email, phone, address_id) VALUES ('$fullName', '$fullName', '$email', '$phone', $addressId);\n";
+foreach ($firstNames as $firstName) {
+    foreach ($lastNames as $lastName) {
+        $email = strtolower($firstName . '.' . $lastName . '@' . $domains[array_rand($domains)]);
+        $phone = '555-' . rand(100, 999) . '-' . rand(1000, 9999);
+        $addressId = rand(1, count($streetTypes) * count($streetNames));
+        $customerData .= "INSERT INTO customer (first_name, last_name, email, phone, address_id) VALUES ('$firstName', '$lastName', '$email', '$phone', $addressId);\n";
+    }
 }
 
+// generate data for orders
 $orderData = '';
 for ($i = 1; $i <= 350; $i++) {
-    $customerId = rand(1, 100);
-    $addressId = rand(1, 150);
+    $customerId = rand(1, count($firstNames) * count($lastNames));
+    $addressId = rand(1, count($streetTypes) * count($streetNames));
     $orderData .= "INSERT INTO `order` (customer_id, address_id) VALUES ($customerId, $addressId);\n";
 }
 
-
+// generate data for product 
 $productData = '';
-for ($i = 1; $i <= 750; $i++) {
-    $productName = generateRandomString(rand(5, 10));
-    $description = generateProductDescription();
-    $weight = generateRandomWeight();
-    $baseCost = generateRandomBaseCost();
-    $productData .= "INSERT INTO product (product_name, description, weight, base_cost) VALUES ('$productName', '$description', '$weight', '$baseCost');\n";
+foreach ($products as $product) {
+    $description = "A $product for you to enjoy";
+    $weight = rand(1, 100) . ' lbs';
+    $baseCost = rand(10, 100) . '.99';
+    $productData .= "INSERT INTO product (product_name, description, weight, base_cost) VALUES ('$product', '$description', '$weight', '$baseCost');\n";
 }
 
-
+// generate data for warehouse 
 $warehouseData = '';
-for ($i = 1; $i <= 25; $i++) {
-    $name = generateRandomString(rand(5, 10)) . ' Warehouse';
-    $addressId = rand(1, 150);
-    $warehouseData .= "INSERT INTO warehouse (name, address_id) VALUES ('$name', $addressId);\n";
+foreach ($cities as $city) {
+    foreach ($states as $state) {
+        $name = $city . ' Warehouse';
+        $addressId = rand(1, count($streetTypes) * count($streetNames));
+        $warehouseData .= "INSERT INTO warehouse (name, address_id) VALUES ('$name', $addressId);\n";
+    }
 }
 
-
+// generate data for order_item 
 $orderItemData = '';
 for ($i = 1; $i <= 550; $i++) {
     $orderId = rand(1, 350);
-    $productId = rand(1, 750);
+    $productId = rand(1, count($products));
     $quantity = rand(1, 10);
-    $price = generateRandomBaseCost();
+    $price = rand(10, 100) . '.99';
     $orderItemData .= "INSERT INTO order_item (order_id, product_id, quantity, price) VALUES ($orderId, $productId, $quantity, '$price');\n";
 }
 
-
+// generate data for product_warehouse 
 $productWarehouseData = '';
 for ($i = 1; $i <= 1250; $i++) {
-    $productId = rand(1, 750);
-    $warehouseId = rand(1, 25);
+    $productId = rand(1, count($products));
+    $warehouseId = rand(1, count($cities) * count($states));
     $productWarehouseData .= "INSERT INTO product_warehouse (product_id, warehouse_id) VALUES ($productId, $warehouseId);\n";
 }
 
-
-$dataSqlContent = $dumpSuperstoreSqlContent . $addressData . $customerData . $orderData . $productData . $warehouseData . $orderItemData . $productWarehouseData;
+// write data for data.sql file
+$dataSqlContent = $addressData . $customerData . $orderData . $productData . $warehouseData . $orderItemData . $productWarehouseData;
 file_put_contents('data.sql', $dataSqlContent);
 
 echo "Data has been generated and written to data.sql file.";
-
+?>
